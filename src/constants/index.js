5b5879ad70dce373c5e078b35d06afaf99794f54
Luohino <aniketsingh821305@gmail.com>
2025-12-01 00:23:04 +0530

    New portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,38 @@
+const flavorlists = [
+  {
+    name: "Chocolate Milk",
+    color: "brown",
+    rotation: "rotate-[-8deg]",
+  },
+  {
+    name: "Stawberry Milk",
+    color: "red",
+    rotation: "rotate-[8deg]",
+  },
+];
+
+const nutrientLists = [
+  { label: "Potassium", amount: "245mg" },
+  { label: "Calcium", amount: "500mg" },
+  { label: "Vitamin A", amount: "176mcg" },
+  { label: "Vitamin D", amount: "5mcg" },
+  { label: "Iron", amount: "1mg" },
+];
+
+const cards = [
+  {
+    src: "/videos/f1.mp4",
+    rotation: "rotate-z-[-10deg]",
+    name: "Madison",
+    img: "/images/p1.png",
+    translation: "translate-y-[-5%]",
+  },
+  {
+    src: "/videos/f2.mp4",
+    rotation: "rotate-z-[4deg]",
+    name: "Alexander",
+    img: "/images/p2.png",
+  },
+];
+
+export { flavorlists, nutrientLists, cards };
