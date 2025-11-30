f11f46ec61805c117d53b21553a5001dc3523f0f
Luohino <aniketsingh821305@gmail.com>
2025-12-01 01:03:53 +0530

    Update VideoPinSection.jsx

--- src/components/VideoPinSection.jsx
+++ src/components/VideoPinSection.jsx
@@ -45,13 +45,13 @@ const VideoPinSection = () => {
         }}
         className="size-full video-box"
       >
-        <video src="/videos/pin-video.mp4" playsInline muted loop autoPlay />
+        <video src="/luohino/videos/pin-video.mp4" playsInline muted loop autoPlay />
 
         <div className="abs-center md:scale-100 scale-200">
-          <img src="/images/circle-text.svg" alt="" className="spin-circle" />
+          <img src="/luohino/images/circle-text.svg" alt="" className="spin-circle" />
           <div className="play-btn">
             <img
-              src="/images/play.svg"
+              src="/luohino/images/play.svg"
               alt=""
               className="size-[3vw] ml-[.5vw]"
             />
